bca9b5a53df697b42f1b808d7c72038be80cbd48
Mina10170 <89662271+Mina10170@users.noreply.github.com>
2021-10-27 22:23:23 +0200

    Update index.js
    If applied, this commit applies document endpoints for a complete REST API

--- index.js
+++ index.js
@@ -60,6 +60,8 @@ let topMovies = [
   },
 ];
 
+// Register New Users.
+let newUsers = [''];
 // GET requests
 
 //Get the home page of myFLix App.
@@ -75,6 +77,56 @@ app.get('/documentation', (req, res) => {
 // Get a list of all movies to the users.
 app.get('/movies', (req, res) => {
   res.json(topMovies);
+});
+
+// Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user.
+app.get('/movies/:title', (req, res) => {
+  res.send(
+    'Successful GET request returning data about a single movies by title to the user.'
+  );
+});
+
+// Return data about a genre (description) by name/title (e.g., “Thriller”).
+app.get('/movies/:title/genre', (req, res) => {
+  res.send('Successful GET request returning data about a genre.');
+});
+
+// Return data about a director (bio, birth year, death year) by name.
+app.get('/movies/director/:name', (req, res) => {
+  res.send('Successful GET request returning data about a director.');
+});
+
+// POST Request.
+
+// Allow new users to register.
+app.post('/newUser', (req, res) => {
+  res.send('Successful POST request user was able to register.');
+});
+
+// Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later)
+app.post('/newUser/:id/favorites', (req, res) => {
+  res.send('Successful POST request movie has been added to favorites.');
+});
+
+// PUT Request.
+
+// Allow users to update their user info (username, password, email, date of birth).
+app.put('/newUser/:id/info', (req, res) => {
+  res.send('Successful PUT request user info updated.');
+});
+
+// DELETE Request.
+
+// Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later)
+app.delete('/newUser/:id/favorites', (req, res) => {
+  res.send(
+    'Successful DELETE request movie has been deleted from user list of favorites.'
+  );
+});
+
+// Allow existing users to deregister
+app.delete('/newUser', (req, res) => {
+  res.send('Successful DELETE request existing user has been deregistered.');
 });
 
 // listen for requests
