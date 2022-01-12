41dcbffb5ea5c76417b8aaf8decad7b99d02d796
Mina10170 <89662271+Mina10170@users.noreply.github.com>
2022-01-12 21:07:53 +0100

    Fixing login issue

--- index.js
+++ index.js
@@ -29,12 +29,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
 //let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-app.use(cors());
-
-let auth = require('./auth')(app);
 const passport = require('passport');
 app.use(passport.initialize());
 require('./passport');
+app.use(cors());
+
+let auth = require('./auth')(app);
+
 
 // USE and GET requests
 
